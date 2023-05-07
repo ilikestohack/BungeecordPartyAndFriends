c33abb17cde13289597123ed7536bc2a32bbd8f5
charmines <47259933+ilikestohack@users.noreply.github.com>
2023-05-07 09:51:47 -0400

    fix: PAFMiniGameCommandHandler
    Fixed the PAFMiniGameCommandHandler to use our server management system to run commands as players without 1.19 issues and added command arguments

--- src/main/java/de/simonsator/partyandfriends/main/listener/PAFMiniGameCommandHandler.java
+++ src/main/java/de/simonsator/partyandfriends/main/listener/PAFMiniGameCommandHandler.java
@@ -12,10 +12,13 @@ import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import net.ME1312.SubServers.Sync.SubAPI;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PAFMiniGameCommandHandler implements Listener {
 	private final Set<String> COMMANDS = new HashSet<>();
@@ -26,28 +29,50 @@ public class PAFMiniGameCommandHandler implements Listener {
 			COMMANDS.add(command.toLowerCase(Locale.ROOT));
 	}
 
+	private void runCommandOnPlayers(ChatEvent pEvent){
+		if (pEvent.getSender() instanceof ProxiedPlayer) {
+			OnlinePAFPlayer player =
+					PAFPlayerManager.getInstance().getPlayer((ProxiedPlayer) pEvent.getSender());
+			PlayerParty party = player.getParty();
+			if (party != null && party.isLeader(player)) {
+				ServerInfo leaderServer = player.getServer();
+				SubAPI.getInstance().getRemotePlayer(player.getPlayer().getUniqueId()).getServer(server -> {
+					for (OnlinePAFPlayer member : party.getPlayers()) {
+						if (leaderServer.equals(member.getServer())) {
+							if (member.getPlayer().getPendingConnection().getVersion() >= 759) {
+								server.command(player.getPlayer().getUniqueId(), member.getPlayer().getUniqueId(), pEvent.getMessage().substring(1));
+							} else {
+								member.getPlayer().chat(pEvent.getMessage());
+							}
+						}
+					}
+				});
+			}
+		}
+	}
+
 	@EventHandler
 	public void onChat(ChatEvent pEvent) {
 		BukkitBungeeAdapter.getInstance().runAsync(Main.getInstance(), () -> {
 			try {
 				TASK_COUNTER.taskStarts();
-				if (COMMANDS.contains(pEvent.getMessage().toLowerCase(Locale.ROOT))) {
-					if (pEvent.getSender() instanceof ProxiedPlayer) {
-						OnlinePAFPlayer player =
-								PAFPlayerManager.getInstance().getPlayer((ProxiedPlayer) pEvent.getSender());
-						PlayerParty party = player.getParty();
-						if (party != null && party.isLeader(player)) {
-							ServerInfo leaderServer = player.getServer();
-							for (OnlinePAFPlayer member : party.getPlayers()) {
-								if (leaderServer.equals(member.getServer())) {
-									if (member.getPlayer().getPendingConnection().getVersion() >= 759) {
-										member.sendMessage("The party and friends function 'MiniGameStartingCommands' is not available on minecraft versions 1.19 and up");
-									} else {
-										member.getPlayer().chat(pEvent.getMessage());
-									}
-								}
+				String commandRan = pEvent.getMessage().toLowerCase(Locale.ROOT);
+				AtomicReference<Boolean> commandEqual = new AtomicReference<>(true);
+				for (String COMMAND : COMMANDS) {
+					commandEqual.set(true);
+					String[] commandRanArray = commandRan.split("\\s+");
+					for (int i = 0; i < commandRanArray.length; i++) {
+						String s = commandRanArray[i];
+						if (!s.contains("$arg$")) {
+							String[] commandSplit = COMMAND.split("\\s+");
+							if (commandSplit.length > i && !s.equals(commandSplit[i])) {
+								commandEqual.set(false);
 							}
 						}
+					}
+					if(commandEqual.get()){
+						runCommandOnPlayers(pEvent);
+						break;
 					}
 				}
 			} finally {
